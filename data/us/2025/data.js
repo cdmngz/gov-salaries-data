8f45d9db49771971b8cc76d828013909f74f44b0
Carlos Dominguez <carlos.dominguez1@tui.com>
2025-08-10 02:38:15 +0200

    chore(us-2025): update data

---
+++ data/us/2025/data.js
@@ -0,0 +1,213 @@
+{
+  "baseCurrency": "USD",
+  "president": [
+    {
+      "name": "Donald Trump",
+      "gender": "M",
+      "party": "R",
+      "salary": {
+        "gross": 400000,
+        "net": 260000
+      }
+    }
+  ],
+  "vicePresident": [
+    {
+      "name": "James David Vance",
+      "gender": "M",
+      "party": "R",
+      "salary": {
+        "gross": 235100,
+        "net": 152815
+      }
+    }
+  ],
+  "ministers": [
+    {
+      "name": "Marco Rubio",
+      "gender": "M",
+      "role": "Secretary of State",
+      "party": "R",
+      "salary": {
+        "gross": 250600,
+        "net": 162890
+      },
+      "budget": 64400000000
+    },
+    {
+      "name": "Scott Bessent",
+      "gender": "M",
+      "role": "Secretary of the Treasury",
+      "party": "R",
+      "salary": {
+        "gross": 250600,
+        "net": 162890
+      },
+      "budget": 14400000000
+    },
+    {
+      "name": "Pete Hegseth",
+      "gender": "M",
+      "role": "Secretary of Defense",
+      "party": "R",
+      "salary": {
+        "gross": 250600,
+        "net": 162890
+      },
+      "budget": 850000000000
+    },
+    {
+      "name": "Pam Bondi",
+      "gender": "F",
+      "role": "Attorney General (Department of Justice)",
+      "party": "R",
+      "salary": {
+        "gross": 250600,
+        "net": 162890
+      },
+      "budget": 37800000000
+    },
+    {
+      "name": "Doug Burgum",
+      "gender": "M",
+      "role": "Secretary of the Interior",
+      "party": "R",
+      "salary": {
+        "gross": 250600,
+        "net": 162890
+      },
+      "budget": 17800000000
+    },
+    {
+      "name": "Brooke Rollins",
+      "gender": "F",
+      "role": "Secretary of Agriculture",
+      "party": "R",
+      "salary": {
+        "gross": 250600,
+        "net": 162890
+      },
+      "budget": 29200000000
+    },
+    {
+      "name": "Howard Lutnick",
+      "gender": "M",
+      "role": "Secretary of Commerce",
+      "party": "R",
+      "salary": {
+        "gross": 250600,
+        "net": 162890
+      },
+      "budget": 10100000000
+    },
+    {
+      "name": "Lori Chavez-DeRemer",
+      "gender": "F",
+      "role": "Secretary of Labor",
+      "party": "R",
+      "salary": {
+        "gross": 250600,
+        "net": 162890
+      },
+      "budget": 13900000000
+    },
+    {
+      "name": "Robert F. Kennedy Jr.",
+      "gender": "M",
+      "role": "Secretary of Health and Human Services",
+      "party": "I",
+      "salary": {
+        "gross": 250600,
+        "net": 162890
+      },
+      "budget": 130700000000
+    },
+    {
+      "name": "Scott Turner",
+      "gender": "M",
+      "role": "Secretary of Housing and Urban Development",
+      "party": "R",
+      "salary": {
+        "gross": 250600,
+        "net": 162890
+      },
+      "budget": 72600000000
+    },
+    {
+      "name": "Sean Duffy",
+      "gender": "M",
+      "role": "Secretary of Transportation",
+      "party": "R",
+      "salary": {
+        "gross": 250600,
+        "net": 162890
+      },
+      "budget": 25400000000
+    },
+    {
+      "name": "Chris Wright",
+      "gender": "M",
+      "role": "Secretary of Energy",
+      "party": "R",
+      "salary": {
+        "gross": 250600,
+        "net": 162890
+      },
+      "budget": 51000000000
+    },
+    {
+      "name": "Linda McMahon",
+      "gender": "F",
+      "role": "Secretary of Education",
+      "party": "R",
+      "salary": {
+        "gross": 250600,
+        "net": 162890
+      },
+      "budget": 82400000000
+    },
+    {
+      "name": "Doug Collins",
+      "gender": "M",
+      "role": "Secretary of Veterans Affairs",
+      "party": "R",
+      "salary": {
+        "gross": 250600,
+        "net": 162890
+      },
+      "budget": 153700000000
+    },
+    {
+      "name": "Kristi Noem",
+      "gender": "F",
+      "role": "Secretary of Homeland Security",
+      "party": "R",
+      "salary": {
+        "gross": 250600,
+        "net": 162890
+      },
+      "budget": 62200000000
+    }
+  ],
+  "congress": [],
+  "senate": [],
+  "officials": [
+    {
+      "name": "Karoline Leavitt",
+      "gender": "F",
+      "role": "White House Press Secretary",
+      "party": "R",
+      "salary": {
+        "gross": 180000,
+        "net": 117000
+      }
+    }
+  ],
+  "parties": {
+    "R": {
+      "name": "Republican Party",
+      "range": 5,
+      "color": "#E81A23"
+    }
+  }
+}
